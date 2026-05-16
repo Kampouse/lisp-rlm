db8e377e689d9fb69f93ff85ffbdf7aa6df97ba3
asil <newsetup@asils-MacBook-Air.local>
2026-05-16 13:58:46 -0400

    Weather checker P2 - static output

--- weather.lisp
+++ weather.lisp
@@ -1,6 +1,2 @@
-;; Weather checker for OutLayer
-;; Input (stdin): {"city":"Montreal"}
-;; Output (stdout): raw weather string from wttr.in
-
 (define (run)
-  (http-get "https://wttr.in/Montreal?format=%t"))
+  (print "{\"city\":\"Montreal\",\"warm\":false,\"temp_c\":5}"))
